17088d3692e949ccbe9b6acf5d457f74230ab54d
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-17 23:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = [
+  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+];
+
+const callback = function (el, i, arr) {
+  console.log(el.name);
+};
+ 
+drivers.forEach(callback);
+
+function logDriverNames() {  
+}
+
+function logDriversByHometown() {
+  
+}
+
+function driversByRevenue() {
+  
+}
+ 
+function totalRevenue() {
+  
+} 
+
+function averageRevenue() {
+  
+} 
